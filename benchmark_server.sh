fb3d8de5c4b283dd74098c4cc4270f9082387e63
petcupaula <petcu.paula@gmail.com>
2017-08-11 10:12:07 +0200

    Create benchmark_server.sh

---
+++ benchmark_server.sh
@@ -0,0 +1,16 @@
+
+# View CPU info
+cat /proc/cpuinfo
+
+# Lists hardware in a compact format
+sudo lshw -short
+# Lists all disks and storage controllers in the system
+sudo lshw -class disk -class storage
+
+# Check what type of disk is mounted
+# You should get 1 for hard disks and 0 for a SSD.
+cat /sys/block/sda/queue/rotational
+cat /sys/block/sdb/queue/rotational
+
+# Another cmd to check what type of disk is mounted
+lsblk -d -o name, rota
