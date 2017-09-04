1fa6ce314ea0bf069cf1570449a40f3a00276784
petcupaula <petcu.paula@gmail.com>
2017-09-04 14:08:37 +0200

    Fix for issue on opening study from browse in analyze tab

---
+++ update_rwgsearch_for_browse_to_analyze.sh
@@ -0,0 +1,16 @@
+# Issue: The 'Open in Analyze View' functionality from the Browse tab does not filter on the study in the Analyze tab
+# Cause: A search is performed on the selected study, however one of the parameters of the search is not encoded correctly. 
+# Solution: Update the JavaScript code that creates the query params in order to encode correctly
+
+cd /var/lib/tomcat7/webapps/transmart/js
+sudo nano rwgsearch.js
+
+# Update the following:
+# Change the following line from the showFacetResults function:
+# queryString += "&searchTerms=" + encodeURIComponent(savedSearchTerms) + "&searchOperators=" + operatorString + "&globaloperator=" + globalLogicOperator;
+# to:
+# queryString += "&searchTerms=" + encodeURIComponent(savedSearchTerms) + "&searchOperators=" + encodeURIComponent(operatorString) + "&globaloperator=" + globalLogicOperator;
+# Save changes
+
+# Restart the tomcat server and refresh cached resources in the browser.
+sudo service tomcat7 restart
