15d3ccc65c2892bda1b84cc83d54fa66926383f1
petcupaula <petcu.paula@gmail.com>
2017-09-05 15:52:29 +0200

    Create GWAS_3_filter_selection.sh

---
+++ post-install-fixes/GWAS_3_filter_selection.sh
@@ -0,0 +1,13 @@
+# Issue: filters selected in the popup in the GWAS module are not applied and result in an error
+# Cause: the Console reports that the addSearchTerm is not defined
+# The general issue appears to be the naming of the functions
+
+cd /var/lib/tomcat7/webapps/transmart/js/facetedSearch
+nano facetedSearchBrowse.js
+# Change the call in the function applyPopupFiltersAnalyses() from addSearchTerm to gwasAddSearchTerm
+# Change the call in the function applyPopupFiltersDataTypes() from addSearchTerm to gwasAddSearchTerm
+
+cd /var/lib/tomcat7/webapps/transmart/plugins/transmart-gwas-16.2/js
+nano gwas.js
+# Change the call in the function gwasShowSearchTemplate from isDataCategory to gwasIsDataCategory
+
