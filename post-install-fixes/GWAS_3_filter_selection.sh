ef63ffb390f53922fc04af871aaf8a6df6e2d4b8
petcupaula <petcu.paula@gmail.com>
2017-09-06 16:10:05 +0200

    Update GWAS_3_filter_selection.sh

--- post-install-fixes/GWAS_3_filter_selection.sh
+++ post-install-fixes/GWAS_3_filter_selection.sh
@@ -1,6 +1,10 @@
 # Issue: filters selected in the popup in the GWAS module are not applied and result in an error
-# Cause: the Console reports that the addSearchTerm is not defined
-# The general issue appears to be the naming of the functions
+# Cause: the Console reports that the addSearchTerm is not defined for the Data type and Analyses filter,
+# and the issue appears to be the naming of the functions
+# For the Region of Interest filter, the reported error is a bad request because the query is not encoded
+# properly. 
+# For the Study filter, the error is 404 not found, since it makes a request for browseGWASExperimentsMultiSelect
+# and it can't find it.
 
 cd /var/lib/tomcat7/webapps/transmart/js/facetedSearch
 nano facetedSearchBrowse.js
