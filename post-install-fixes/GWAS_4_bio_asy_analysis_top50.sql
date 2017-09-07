c47272c7c1db74be3644e2cb77f6a1aad4912a9a
petcupaula <petcu.paula@gmail.com>
2017-09-07 15:02:15 +0200

    Add fix for eQTL table

--- post-install-fixes/GWAS_4_bio_asy_analysis_top50.sql
+++ post-install-fixes/GWAS_4_bio_asy_analysis_top50.sql
@@ -2,6 +2,7 @@
 -- (org.postgresql.util.PSQLException ERROR: column "beta" does not exist Position: 118)
 -- Solution: the beta column is missing in the biomart.bio_asy_analysis_gwas_top50 table
 -- so we create the table again, with the expected columns
+-- A similar issue occurs for eQTL data, giving an error on colum "gene" missing
 
 -- Connect to the server and run postgres commands to recreate the table
 -- cd /datastore/postgresql/tablespaces/
@@ -42,3 +43,38 @@ ALTER TABLE biomart.bio_asy_analysis_gwas_top50
 GRANT ALL ON TABLE biomart.bio_asy_analysis_gwas_top50 TO biomart;
 GRANT ALL ON TABLE biomart.bio_asy_analysis_gwas_top50 TO tm_cz;
 GRANT SELECT ON TABLE biomart.bio_asy_analysis_gwas_top50 TO biomart_user;
+
+
+-- Table: biomart.bio_asy_analysis_eqtl_top50
+
+DROP TABLE biomart.bio_asy_analysis_eqtl_top50;
+
+CREATE TABLE biomart.bio_asy_analysis_eqtl_top50
+(
+  bio_assay_analysis_id bigint,
+  analysis character varying(500),
+  chrom character varying(4),
+  pos numeric(10,0),
+  rsgene character varying(200),
+  rsid character varying(50),
+  pvalue double precision,
+  logpvalue double precision,
+  extdata character varying(4000),
+  rnum bigint,
+  intronexon character varying(10),
+  regulome character varying(10),
+  recombinationrate numeric(18,6),
+  gene character varying(200),
+  strand smallint
+)
+WITH (
+  OIDS=FALSE
+)
+TABLESPACE transmart;
+ALTER TABLE biomart.bio_asy_analysis_eqtl_top50
+  OWNER TO biomart;
+GRANT ALL ON TABLE biomart.bio_asy_analysis_eqtl_top50 TO biomart;
+GRANT ALL ON TABLE biomart.bio_asy_analysis_eqtl_top50 TO tm_cz;
+GRANT SELECT ON TABLE biomart.bio_asy_analysis_eqtl_top50 TO biomart_user;
+
+-- To exit from psql console type \q
