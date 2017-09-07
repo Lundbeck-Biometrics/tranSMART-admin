555d41a6ea4b0a036e50b82b2427f70e6e063c65
petcupaula <petcu.paula@gmail.com>
2017-09-07 15:41:44 +0200

    Create analyze-fix.sh

---
+++ post-install-fixes/analyze-fix.sh
@@ -0,0 +1,13 @@
+-- Issue: Getting bad request error in console when expanding notes in Dataset Explorer
+-- Solution: Update datasetExplorer.js to encode the request
+
+cd /var/lib/tomcat7/webapps/transmart/js/datasetExplorer
+sudo nano datasetExplorer.js
+
+# Update in function setupOntTree:
+# from 
+# url: addNodeDseURL + "?node=" + node.id
+# to
+# url: addNodeDseURL + "?node=" + encodeURIComponent(node.id)
+
+# Save and restart tomcat and refresh browser cache
