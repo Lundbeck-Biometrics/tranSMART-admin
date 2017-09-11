c3239c135f612165b1b1af4a5488c8f77256e458
petcupaula <petcu.paula@gmail.com>
2017-09-11 18:27:19 +0200

    Update clean_old_data.sh

--- clean_old_data.sh
+++ clean_old_data.sh
@@ -1,11 +1,16 @@
 # Might be a good idea to delete the old output R job analyses from time to time
-# TODO: Consider creating a cron task for this
 
 # To check how much space they occupy at the moment:
 du -hs /datastore/jobs
 
 # To remove all jobs
 cd /datastore/jobs
-sudo rm -r *
+# sudo rm -r *
+
+# TODO: the above will remove everything from the folder, but we actually don't 
+# want to remove the cachedQQPlotImages and cachedManhattanplotImages folders 
+# as they will not automatically be created and are needed for the GWAS module
 
 # TODO: example of removing the oldest jobs only
+
+# TODO: Consider creating a cron task for this
