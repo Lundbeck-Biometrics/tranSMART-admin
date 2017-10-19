62289a3fff176c28976bf92a8cb180a96d112871
Paula Petcu <petcu.paula@gmail.com>
2017-10-19 15:18:46 +0200

    Update GWAS_6_filtering.sh

--- post-install-fixes/GWAS_6_filtering.sh
+++ post-install-fixes/GWAS_6_filtering.sh
@@ -1,8 +1,9 @@
 # Workaround for filling in the pvalue from the data loaded in the field p_value_char
 
-update biomart.bio_assay_analysis_eqtl
-set p_value=cast(p_value_char as decimal);
+#update biomart.bio_assay_analysis_eqtl
+#set p_value=cast(p_value_char as decimal);
 
-update biomart.bio_assay_analysis_gwas
-set p_value=cast(p_value_char as decimal);
+#update biomart.bio_assay_analysis_gwas
+#set p_value=cast(p_value_char as decimal);
 
+# TO-DO: upload the actual value in the ETL instead of this post-load update
