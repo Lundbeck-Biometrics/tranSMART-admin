0eb89242e452a74acfa98015b88bbe80cbcf8707
Paula Petcu <petcu.paula@gmail.com>
2018-02-01 21:14:42 +0100

    Update fix_for_multiple_snpdb_versions.sql

--- customisations/fix_for_multiple_snpdb_versions.sql
+++ customisations/fix_for_multiple_snpdb_versions.sql
@@ -11,3 +11,7 @@ ADD COLUMN dbsnp_version character varying(10);
 ALTER TABLE DEAPP.DE_RC_SNP_INFO 
 ADD COLUMN dbsnp_version character varying(10);
 
+-- Add dbSNP version column to bio_assay_analysis_ext (used for showing metadata in interface)
+
+ALTER TABLE BIOMART.BIO_ASSAY_ANALYSIS_EXT
+ADD COLUMN dbsnp_version character varying(10);
