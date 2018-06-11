0d6983f976cfada94da6129d04bcd357ced8a877
petcupaula <petcu.paula@gmail.com>
2018-06-11 12:41:01 +0200

    Add changes made to Config file on dev branch

--- patching/customisations/Config.groovy
+++ patching/customisations/Config.groovy
@@ -155,7 +155,6 @@ environments { development {
 // Whether to enable guest auto login.
 // If it's enabled no login is required to access tranSMART.
 com.recomdata.guestAutoLogin = true
-environments { development { com.recomdata.guestAutoLogin = true } }
 
 // Guest account user name - if guestAutoLogin is true, this is the username of
 // the account that tranSMART will automatically authenticate users as. This will
@@ -297,6 +296,9 @@ buildInfo { properties {
 
 /* }}} */
 
+// Enable cross origin for glowing bear frontend.
+grails.cors.enabled = true
+
 /* {{{ Spring Security configuration */
 
 grails { plugin { springsecurity {
@@ -306,8 +308,8 @@ grails { plugin { springsecurity {
         def glowingBearRedirectUris = [
                 org.transmartproject.app.transmartURL - ~/transmart\/?$/ + 'connections',
         ]
-        // for dev, node reverse proxy runs on 8001
-        glowingBearRedirectUris << 'http://localhost:8001/connections'
+        // The glowing bear runs on port 4200 by default
+        glowingBearRedirectUris << 'http://localhost:4200'
 
         oauthProvider {
             authorization.requireRegisteredRedirectUri = true
